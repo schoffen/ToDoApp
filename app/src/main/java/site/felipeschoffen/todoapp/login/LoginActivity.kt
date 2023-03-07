8c1ad6c387995bb53f8e238f4fc1562d0023d586
Felipe <fschoffen@yahoo.com>
2023-03-06 22:28:32 -0300

    Initial commit, login view

---
+++ app/src/main/java/site/felipeschoffen/todoapp/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package site.felipeschoffen.todoapp.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import site.felipeschoffen.todoapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
