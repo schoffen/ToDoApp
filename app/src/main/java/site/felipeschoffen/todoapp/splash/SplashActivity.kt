b98e228d85a85a2f80362e0120e0ab78290860bc
Felipe <fschoffen@yahoo.com>
2023-03-07 00:25:37 -0300

    login view

---
+++ app/src/main/java/site/felipeschoffen/todoapp/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package site.felipeschoffen.todoapp.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import site.felipeschoffen.todoapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
