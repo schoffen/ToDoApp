f2c942a21e050c097c965efc2881a674de332c9c
Felipe <fschoffen@yahoo.com>
2023-03-07 10:23:10 -0300

    fixed button layout behavior, fixed edit text behavior, add scroll when edit text focus

--- app/src/main/java/site/felipeschoffen/todoapp/login/LoginActivity.kt
+++ app/src/main/java/site/felipeschoffen/todoapp/login/LoginActivity.kt
@@ -2,11 +2,18 @@ package site.felipeschoffen.todoapp.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.content.res.AppCompatResources
 import site.felipeschoffen.todoapp.R
+import site.felipeschoffen.todoapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginLoginBtn.background = AppCompatResources.getDrawable(this, R.drawable.button)
     }
 }
